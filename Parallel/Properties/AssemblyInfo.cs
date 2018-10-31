f1f54c595a47dfc02b9fa023aa0e88a7a74517e5
vgadev <43451257+vgadev@users.noreply.github.com>
2018-10-30 17:04:13 -0700

    Adding the code.

--- Parallel/Properties/AssemblyInfo.cs
+++ Parallel/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Parallel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A parallel task runner")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Parallel")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright vgadev © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
